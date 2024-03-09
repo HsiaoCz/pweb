5f4c89f65362a49ef745869780f40cd91b60031a
HsiaoCz <984274788@qq.com>
2024-03-09 08:09:00 +0800

    pweb flask

---
+++ flask/requets/requestobj.py
@@ -0,0 +1,26 @@
+# request 包含前端发送过来的所有请求
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return "Hello"
+
+
+@app.route("/index", methods=["POST"])
+def index():
+    if request.method == "POST":
+        return request.remote_addr
+
+
+@app.route("/user", methods=["POST"])
+def user():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    print(username, password)
+    return "ok"
