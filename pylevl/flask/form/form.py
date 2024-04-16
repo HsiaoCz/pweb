ac38f6a651b6614fb499878e6742a5532cfad4a9
HsiaoCz <984274788@qq.com>
2024-04-16 08:22:05 +0800

    python

---
+++ pylevl/flask/form/form.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+# 实例化一个app
+app = Flask(__name__)
+
+
+@app.route("/index", methods=["POST", "GET"])
+def index():
+    return render_template("template/index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
