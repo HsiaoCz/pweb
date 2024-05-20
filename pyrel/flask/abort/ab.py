19b507fa1de99b4d5d3b60f35e6e0649b370c5a1
HsiaoCz <984274788@qq.com>
2024-05-21 06:10:17 +0800

    some recall

---
+++ pyrel/flask/abort/ab.py
@@ -0,0 +1,25 @@
+# abort 函数
+# 在网页当中 主动抛出异常
+from flask import Flask
+from flask import abort
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    if request.method.get("GET"):
+        return make_response("index.html")
+    if request.method.get("POST"):
+        name = request.form.get("name")
+        password = request.form.get("password")
+        if name == "zhangsan" and password == "123":
+            return "login success"
+        else:
+            abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
