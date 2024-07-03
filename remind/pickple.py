7835c9a571d30e05a78d9379417f363d968e0b3c
HsiaoCz <984274788@qq.com>
2024-07-04 02:18:50 +0800

    add some

---
+++ remind/pickple.py
@@ -0,0 +1,17 @@
+# pickple的用法和json模块相同，不同点在于它只能用于python
+# 可以序列化python独有数据类型
+import pickle
+
+
+# 序列化
+res = pickle.dumps({1, 2, 3})
+
+# 序列化 集合
+print(res)
+
+# 反序列化
+res1 = pickle.loads(res)
+
+print(res1)
+
+
